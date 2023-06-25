c511b2ed09bf590874cb93f8e915c144484c6fd9
Ali Donmez <aldonmez@gmail.com>
2023-06-25 12:38:09 -0500

    updates

--- retrievalAugmentedGen_typescript/main.ts
+++ retrievalAugmentedGen_typescript/main.ts
@@ -1,61 +1,39 @@
-import {OpenAI} from "langchain/llms/openai";
-import {ConversationalRetrievalQAChain} from "langchain/chains";
 import {TextLoader} from "langchain/document_loaders/fs/text";
 import {TokenTextSplitter} from "langchain/text_splitter";
 import {Document} from "langchain/document";
-import {load} from "langchain/load";
 import {OpenAIEmbeddings} from "langchain/embeddings";
-import {Chroma} from "langchain/vectorstores";
 import {FaissStore} from "langchain/vectorstores/faiss";
 
-class RAG {
-    // private field
-    #radius;
-
-    constructor(value: String) {
-        // You can access private field from constructor
-        this.#radius = value;
+class RetrievalAugmentedGeneration {
+    constructor() {
     }
 
-    private async loadandSplitDoc(): Promise<Document[]> {
+    private async loadAndSplitDoc(): Promise<Document[]> {
         const loader = new TextLoader("src/docs/qa.txt");
-        const textSplitter = new TokenTextSplitter({chunkSize: 100, chunkOverlap: 10})
+        const textSplitter = new TokenTextSplitter({ chunkSize: 100, chunkOverlap: 10 });
         return await loader.loadAndSplit(textSplitter);
     }
 
-    private newEmbeddings() {
-        return new OpenAIEmbeddings;
+    private newEmbeddings(): OpenAIEmbeddings {
+        return new OpenAIEmbeddings();
     }
 
-    public async createAndSaveFaissVectors() {
-        process.env.OPENAI_API_KEY = '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'
-
-        const docs = await this.loadandSplitDoc()
-        const vectorStore = await FaissStore.fromDocuments(
-            docs,
-            new OpenAIEmbeddings()
-        );
-        const directory = "faiss_vectors/";
-        await vectorStore.save(directory);
-
+    public async createAndSaveFaissVectors(vectorStoreDirectory: string): Promise<void> {
+        const docs = await this.loadAndSplitDoc();
+        const vectorStore = await FaissStore.fromDocuments(docs, this.newEmbeddings());
+        await vectorStore.save(vectorStoreDirectory);
     }
 
-    public async similaritySearch(vectorStoreDirectory: string) {
-        process.env.OPENAI_API_KEY = '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'
-
-        const loadedVectorStore = await FaissStore.load(
-            vectorStoreDirectory,
-            new OpenAIEmbeddings()
-        );
-
-        return await loadedVectorStore.similaritySearch("book ticket local currency", 1);
+    public async similaritySearch(question: string, vectorStoreDirectory: string): Promise<any> {
+        const loadedVectorStore = await FaissStore.load(vectorStoreDirectory, this.newEmbeddings());
+        return await loadedVectorStore.similaritySearch(question, 1);
     }
 }
 
-const main = async () => {
-    let testrag = new RAG('test')
-    let search = await testrag.similaritySearch("faiss_vectors/")
+const main = async (): Promise<void> => {
+    const retrievalAugGen = new RetrievalAugmentedGeneration();
+    const search = await retrievalAugGen.similaritySearch("book ticket local currency","faiss_vectors/");
     console.log(search);
-}
+};
 
 await main();
